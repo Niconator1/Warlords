efed871c25ed02e537d2e452dbd3a2b8d6d42298
Niconator1 <1niconator1@gmail.com>
2016-07-10 17:11:02 +0200

    Fixed eps and health

--- Warlords/src/com/warlords/util/type/Thunderlord.java
+++ Warlords/src/com/warlords/util/type/Thunderlord.java
@@ -15,10 +15,10 @@ import com.warlords.util.itemlist.ItemListThunder;
 public class Thunderlord extends SpielKlasse {
 	public static String name = "Thunderlord";
 	private static String description = "";
-	private static int eps = 7;
+	private static int eps = 10;
 	private static int eph = 10;
 	public static int epmax = 200;
-	public static int hpmax = 6750;
+	public static int hpmax = 6000;
 	// Lightningbolt
 	public static final String boltname = "Lightning Bolt";
 	public double dminbolt = 215;
