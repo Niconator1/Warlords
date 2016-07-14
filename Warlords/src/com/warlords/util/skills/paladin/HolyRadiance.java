c2130989e988ef94095ba2ed71651d2dde6d8ddc
Niconator1 <1niconator1@gmail.com>
2016-07-14 21:11:06 +0200

    Added Holy/Unholy Radiance Team heal, Fixed Time Warp heal

---
+++ Warlords/src/com/warlords/util/skills/paladin/HolyRadiance.java
@@ -0,0 +1,52 @@
+package com.warlords.util.skills.paladin;
+
+import org.bukkit.Location;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Player;
+
+public class HolyRadiance {
+	private ArmorStand a;
+	private Player shooter;
+	private Player target;
+	private double hmin;
+	private double hmax;
+	private double critc;
+	private double critm;
+	private Location start;
+	public HolyRadiance(ArmorStand f, double hmin, double hmax, double critc,
+			double critm, Player shooter, Player target, Location l) {
+		this.a=f;
+		this.hmin=hmin;
+		this.hmax=hmax;
+		this.critc=critc;
+		this.critm=critm;
+		this.shooter=shooter;
+		this.target=target;
+		this.start=l;
+	}
+	public ArmorStand getStand(){
+		return a;
+	}
+	public Player getShooter(){
+		return shooter;
+	}
+	public double hmin(){
+		return hmin;
+	}
+	public double hmax(){
+		return hmax;
+	}
+	public double critc(){
+		return critc;
+	}
+	public double critm(){
+		return critm;
+	}
+	public Location start(){
+		return start;
+	}
+	public Player getTarget(){
+		return target;
+	}
+	
+}
